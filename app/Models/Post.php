ff62718ca9459b006062be23d6f26dda6c6252fd
OsamaNagi <alshahidyosama@gmail.com>
2023-07-01 18:27:56 +0200

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $guarded = []; // This is a security feature to prevent mass assignment vulnerability
+    protected $with = ['category', 'author']; // This is a performance feature to prevent N+1 query problem
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
